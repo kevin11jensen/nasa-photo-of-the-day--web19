18f1764d588c3714a2b9b048f47c9c8f370718ae
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-28 17:56:36 -0700

    found the data from axios call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+
+  const [state, setState] = useState([])
+
+  useEffect(() =>{
+    axios.get('https://api.nasa.gov/planetary/apod?date=2020-07-28&api_key=A1P17AHxULNnREkbWMXMRvDVt7CozildTqHKF2mT')
+    .then(res => {
+      console.log('response:', res.data)
+      setState(res.data)
+    })
+    .catch(err => {
+      console.log('error:', err)
+    })
+  }, [])
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        hello From App
       </p>
     </div>
   );
