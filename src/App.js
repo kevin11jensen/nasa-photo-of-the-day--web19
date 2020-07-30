7dde600db8890c8e420504be1455b4f9ca1fe246
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-29 19:50:08 -0700

    attempting a change day button

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
 import NasaCard from './components/NasaCard';
-
+import styled from 'styled-components';
 
 
 function App() {
 
   const [state, setState] = useState([])
+  const [date, setDate] = useState('2020-07-28')
 
   useEffect(() =>{
-    axios.get('https://api.nasa.gov/planetary/apod?date=2020-07-28&api_key=A1P17AHxULNnREkbWMXMRvDVt7CozildTqHKF2mT')
-    .then(res => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=A1P17AHxULNnREkbWMXMRvDVt7CozildTqHKF2mT`)
+      .then(res => {
       console.log('response:', res.data)
       setState(res.data)
     })
@@ -21,15 +23,29 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <NasaCard 
-      title = {state.title}
-      hdurl = {state.hdurl}
-      date = {state.date}
-      explanation = {state.explanation}
-      />
-    </div>
+    <MainContainer>
+      <div className="App">
+        <NasaCard 
+        title = {state.title}
+        hdurl = {state.hdurl}
+        date = {state.date}
+        explanation = {state.explanation}
+        />
+        <button onClick = {() => setDate()}>Next Day</button>
+      </div>
+    </MainContainer>
   );
 }
 
 export default App;
+
+const MainContainer = styled.div `
+  margin: 0;
+  .App {
+    button {
+      position: absolute;
+      left: 1%;
+      bottom: 3%;
+    }
+  }
+`
